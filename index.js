bb8c15a5795262bd56500ad90708ba0123580667
Sam <sam.camhi@gmail.com>
2023-03-26 11:28:29 -0400

    Passed 1st Class Functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0,2);
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(drivers.length - 2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (mult) => (fare) => fare*mult;
+
+const fareDoubler = (fare) => createFareMultiplier(2)(fare);
+
+const fareTripler = (fare) => createFareMultiplier(3)(fare);
+
+const selectDifferentDrivers = (drivers, which) => which(drivers);
